76a43154e0980accbe9c1f1af42bc82d7b2db3c8
Muhammad Taha <ch.tahaarif2005@gmail.com>
2026-07-20 17:50:40 +0500

    fixing next.config.js

--- next.config.js
+++ next.config.js
@@ -1,12 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  // Enforces clean trailing slash configurations for edge crawlers
   trailingSlash: false,
 
-  // Tells Next.js to build a standard static HTML/CSS/JS architecture
-  output: 'export', 
-
-  // Required for static exports because standard dynamic node image resizing requires a live server
   images: {
     unoptimized: true,
   },
